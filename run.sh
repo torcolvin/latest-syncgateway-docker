c6a5b16b3216d117fb73776c055652c7ca342888
Tor Colvin <tor.colvin@couchbase.com>
2023-03-24 13:46:27 -0400

    Uncomment line

--- run.sh
+++ run.sh
@@ -6,7 +6,7 @@ SG_VERSION=570
 
 SG_DEB=couchbase-sync-gateway-enterprise_3.1.0-${SG_VERSION}_aarch64.deb
 
-#curl -L -O http://latestbuilds.service.couchbase.com/builds/latestbuilds/sync_gateway/3.1.0/$SG_VERSION/$SG_DEB
+curl -L -O http://latestbuilds.service.couchbase.com/builds/latestbuilds/sync_gateway/3.1.0/$SG_VERSION/$SG_DEB
 
 docker build --build-arg "SG_DEB=$SG_DEB" --tag sync_gateway_latest .
 
